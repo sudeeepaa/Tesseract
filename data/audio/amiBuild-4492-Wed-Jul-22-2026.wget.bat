b20d1dffd706f9c52c19d6b25a47b8b240f15954
sudeeepaa <sudeepasanthanam2004@gmail.com>
2026-07-22 21:24:43 +0530

    import audio dataset, license documentation, and configuration scripts

---
+++ data/audio/amiBuild-4492-Wed-Jul-22-2026.wget.bat
@@ -0,0 +1,4 @@
+
+wget    -P amicorpus/ES2002a/audio https://groups.inf.ed.ac.uk/ami/AMICorpusMirror//amicorpus/ES2002a/audio/ES2002a.Mix-Headset.wav
+wget  https://groups.inf.ed.ac.uk/ami/download/temp/amiBuild-4492-Wed-Jul-22-2026.manifest.txt
+wget  https://groups.inf.ed.ac.uk/ami/download/temp/../CCBY4.0.txt
